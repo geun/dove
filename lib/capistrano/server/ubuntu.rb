ba574ed2d9cc2ff366218feb2255c1941a8dad9d
geun <geun@chattingcat.com>
2015-09-13 13:47:11 +0900

    include ntp service

--- lib/capistrano/server/ubuntu.rb
+++ lib/capistrano/server/ubuntu.rb
@@ -1,3 +1,4 @@
 load File.expand_path("../tasks/ubuntu.rake", __FILE__)
 load File.expand_path("../tasks/fail2ban.rake", __FILE__)
 load File.expand_path("../tasks/iptables.rake", __FILE__)
+load File.expand_path("../tasks/ntp.rake", __FILE__)
